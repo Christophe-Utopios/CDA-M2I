db8259874ea8523fa2dd66ec3aba9d83880f5ce1
Christophe Ringot <christophe.r@utopios.solutions>
2023-09-06 12:06:09 +0200

    Suite demo

--- Javascript/01-Cours/02_OperateursArithmetiques/script.js
+++ Javascript/01-Cours/02_OperateursArithmetiques/script.js
@@ -24,3 +24,33 @@ console.log(`${nb1} / ${nb2} = ${resultat}`);
 // Modulo (reste de la division)
 resultat = nb1 % nb2
 console.log(`${nb1} % ${nb2} = ${resultat}`);
+
+console.log("Les opérateurs d'incrémentation/décrémentation");
+
+// Incrémentation
+nb1 = nb1 + 1 // Incrémentation classique
+console.log(nb1);
+nb1 += 1 // Opérateur combiné +=
+console.log(nb1);
+nb1++ // Opérateur d'incrémentation
+console.log(nb1);
+
+// Décrémentation
+nb1 = nb1 - 1
+console.log(nb1);
+nb1 -= 1
+console.log(nb1);
+nb1--
+console.log(nb1);
+
+console.log("----------------");
+
+// Incrémentation puis lecture de la valeur
+console.log(++nb1);
+// Lecture de la valeur puis incrémentation
+console.log(nb1++);
+
+// Multiplication combinée
+console.log(nb2 *= nb1);
+// Division combinée
+console.log(nb2 /= nb1);
