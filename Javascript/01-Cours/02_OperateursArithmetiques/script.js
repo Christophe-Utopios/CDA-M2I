68189efbc22ce086010b48abb3e4a07a5065c7e9
Christophe Ringot <christophe.r@utopios.solutions>
2023-09-06 10:27:43 +0200

    demo js

---
+++ Javascript/01-Cours/02_OperateursArithmetiques/script.js
@@ -0,0 +1,26 @@
+/*
+    Les opérateurs arithmétiques
+*/
+
+let nb1 = 10, nb2 = 65, resultat
+
+// L'addition
+resultat = nb1 + nb2
+console.log(resultat);
+console.log(`${nb1} + ${nb2} = ${resultat}`);
+
+// La soustraction
+resultat = nb1 - nb2
+console.log(`${nb1} - ${nb2} = ${resultat}`);
+
+// Les multiplications
+resultat = nb1 * nb2
+console.log(`${nb1} x ${nb2} = ${resultat}`);
+
+// Division
+resultat = nb1 / nb2
+console.log(`${nb1} / ${nb2} = ${resultat}`);
+
+// Modulo (reste de la division)
+resultat = nb1 % nb2
+console.log(`${nb1} % ${nb2} = ${resultat}`);
