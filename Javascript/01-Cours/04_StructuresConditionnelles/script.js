033161b79f099c970f637247328565041a931909
Christophe Ringot <christophe.r@utopios.solutions>
2023-09-07 17:24:35 +0200

    correction exo 18 -19

--- Javascript/01-Cours/04_StructuresConditionnelles/script.js
+++ Javascript/01-Cours/04_StructuresConditionnelles/script.js
@@ -37,6 +37,12 @@ if(estVrai){
     console.log("C'est faux");
 }
 
+/*
+    Opérateur logique && => ET
+    Operateur logique || => OU
+    Operateur logique ! => contraire de ...
+*/
+
 console.log(1 === 1); // Vrai
 console.log("1" === 1); // Faux
 console.log(1 == 1); // Vrai
@@ -44,3 +50,54 @@ console.log("1" == 1); // Vrai
 console.log("1" != 1); // Faux
 console.log(3 !== 3); // Faux
 console.log(4 !== 3); // Vrai
+
+/*
+        switch case
+
+        switch (condition){
+            case valeur1:
+                // instructions
+                break;
+            case valeur2:
+                // instructions
+                break;
+            default:
+                // Instructions
+                break;
+        }
+*/
+
+let fruit = "Banane"
+
+switch (fruit) {
+    case "Oranges":
+        console.log("Les oranges sont à 2 euros");
+        break
+    case "Banane":
+        console.log("Les bananes sont à 1 euro");
+        break
+    default:
+        console.log("Votre fruit n'existe pas !");
+}
+
+// Switch avec range de valeurs
+
+let age = 3
+
+switch (age) {
+    case age <= 6:
+        console.log("Votre enfant est dans la catégorie 'BABY'");
+        break;
+    case age <= 8:
+        console.log(`Votre enfant est dans la catégorie "Poussin"`);
+        break;
+    case age <= 10:
+        console.log(`Votre enfant est dans la catégorie "Pupille"`);
+        break;
+    case age <= 12:
+        console.log(`Votre enfant est dans la catégorie "Minime"`);
+        break
+    default:
+        console.log(`Votre enfant est dans la catégorie "Cadet"`);
+        break
+}
---
+++ Javascript/01-Cours/05_lesBoucles/script.js
@@ -0,0 +1,27 @@
+/*
+    Les boucles
+*/
+
+// Boucle Tant que ... (while)
+
+let i = 1
+
+while (i <= 10) {
+    console.log(`La boucle while s'est exécutée ${i} fois`);
+    i++
+}
+
+// Boucle Faire tant que ... (do while)
+
+let n = 1
+
+do{
+    console.log(`La boucle do while s'est exécutée ${i} fois`);
+    n++
+} while (n <= 10)
+
+// boucle for
+
+for (let k = 0; k < 10; k++){
+    console.log(`La boucle for s'est exécutée ${i} fois`);
+}
