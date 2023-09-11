020de0f4f9f13e9b78111f4a2ae8f15af1c6b6de
christophe-utopios <christophe.r@utopios.solutions>
2023-09-11 13:36:58 +0200

    correction exercices

--- Javascript/01-Cours/05_lesBoucles/script.js
+++ Javascript/01-Cours/05_lesBoucles/script.js
@@ -25,3 +25,51 @@ do{
 for (let k = 0; k < 10; k++){
     console.log(`La boucle for s'est exécutée ${i} fois`);
 }
+
+// break
+
+for (let i = 1; i <= 10; i++){
+    if (i === 5) {
+        break
+    }
+    console.log(i);
+} // Cela affichera les chiffres de 1 à 4
+
+// Continue
+
+for (let i = 1; i <= 10; i++){
+    if (i === 5) {
+        continue
+    }
+    console.log(i);
+} // Cela affichera 1 2 3 4 6 7 8 9 10
+
+
+let numbers = [1,2,3,4,5,6,7]
+
+for (let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}
+
+// For ... of
+
+for(let number of numbers) {
+    console.log(number);
+}
+
+// forEach
+
+numbers.forEach(number => {
+    console.log(number);
+});
+
+// For ... In
+let fruit = {
+    nom: "Banane",
+    prix: 1.05,
+    couleur: "jaune"
+}
+
+for (let key in fruit){
+    console.log(key + " : " + fruit[key]);
+}
