a4201242e84621c1abdd19726d6638648229ee18
christophe <christophe.delory@labom2iformation.fr>
2023-09-20 12:16:59 +0200

    debut demo promesse

--- Javascript/01-Cours/13_Promesse/main.js
+++ Javascript/01-Cours/13_Promesse/main.js
@@ -4,8 +4,8 @@ const p = new Promise((resolve, reject) => {
    //Logique métier dans la promise
    let test = true
     setTimeout(() => {
-     if(!test){
-         resolve({data : "Les données envoyées par la promise"})
+     if(test){
+         resolve({data : "Les données envoyées par la promise", message : "ok"})
      }else {
          reject({message : "Message d'erreur en cas d'echec de la promise"})
      }
@@ -15,11 +15,26 @@ const p = new Promise((resolve, reject) => {
 
 // Flux d'execution
 
-p.then((response) => {
-    result.innerHTML = response.data
-}).catch(err => {
-    result.innerHTML = err.message
-})
+// p.then((response) => {
+//     result.innerHTML = response.data
+// }).catch(err => {
+//     result.innerHTML = err.message
+// })
 
 const result = document.querySelector("#result")
 result.innerHTML = "En cours d'execution"
+
+
+const get_promise_async = async () => {
+    // pour récuperer le rejet on peut utiliser un try catch
+    try {
+        const resultat = await p
+        console.log(resultat)
+        result.innerHTML = resultat.data
+    }catch(ex){
+        console.log(ex)
+        result.innerHTML = ex.message
+    }
+}
+
+get_promise_async()
