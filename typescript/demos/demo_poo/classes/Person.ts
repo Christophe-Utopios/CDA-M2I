f25cb59e1c6676af482e682e0d887c26d782d931
christophe <christophe.delory@labom2iformation.fr>
2023-09-25 15:18:49 +0200

    debut demo poo ts

--- typescript/demos/demo_poo/classes/Person.ts
+++ typescript/demos/demo_poo/classes/Person.ts
@@ -1,5 +1,20 @@
+/*
+  Lorsque l'on veut faire des classes en Typescript, il nous faut modifier un peu la syntaxe
 
-export class Person {
+  Première chose : Il va nous falloir ajouter des niveaux d'accessibilité à nos attributs : 
+    - public : L'attribut / la méthode sera accessible à tous, que cela soit dans la classe, dans les classes enfants, dans les autres classes
+    - protected: L'attribut / la méthode sera accessible à notre classe et à ses enfants
+    - private : L'attribut / la méthode ne sera accessible que dans la classe
+    - readonly: L'attribut sera constant une fois assigné. Il faudra lui affecter une valeur dans le constructeur. Par la suite, il sera publique pour la lecture mais ne pourra pas être modifié
+
+  A côté de cela, le Typescript permet la création d'attributs / de méthodes 'static'. Ces attributs / méthodes seront 
+  disponibles via la syntaxe <Nom de la classe>.<Nom de l'attribut ou de la méthode> 
+  et non de <Nom d'un objet>.<Nom de l'attribut ou de la méthode>
+
+  Les attributs statiques sont particulièrement utiles pour faire un compteur d'éléments créé de cette classe. 
+*/
+
+class Person {
     private static _count: number = 0
     private _id: number;
     private _firstname: string;
@@ -31,9 +46,26 @@ export class Person {
     
       get fullname() {
         return this._firstname + " " + this._lastname
+
       }
     
       static get count() {
         return Person._count
       }
 }
+
+class Student extends Person {
+    private _backpack: boolean;
+
+    constructor(firstname: string, lastname: string, age: number, numeroIdNat: number,backpack: boolean){
+        super(firstname, lastname, age, numeroIdNat)
+        this._backpack = backpack
+    }
+
+    toString(): string {
+        return this.fullname + ` backpack ? ${this._backpack}` 
+    }
+
+}
+
+export {Person, Student}
--- typescript/demos/demo_poo/main.ts
+++ typescript/demos/demo_poo/main.ts
@@ -1,4 +1,4 @@
-import { Person } from "./classes/Person.js";
+import { Person, Student } from "./classes/Person.js";
 
 
 let person: Person;
@@ -8,3 +8,18 @@ person.firstname = "Toto"
 console.log(person.fullname)
 console.log(person.numeroIdNat)
 console.log(`Il y a actuellement ${Person.count} personnes`)
+
+// un tableau de person
+let personArray: Person[] = [];
+
+personArray.push(person)
+
+let myStudent: Student;
+
+myStudent = new Student("titi","tata",45,545215,true)
+
+console.log(myStudent.toString())
+
+console.log(`Il y a actuellement ${Person.count} personnes`)
+
+
