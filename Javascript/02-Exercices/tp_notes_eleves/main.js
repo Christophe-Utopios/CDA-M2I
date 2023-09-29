812d251b735f998a9857c2ce66cc0d419ae5b651
Christophe <christophe.delory@labom2iformation.fr>
2023-09-29 11:51:52 +0200

    corrections exercice notes js et recette ts

--- Javascript/02-Exercices/tp_notes_eleves/main.js
+++ Javascript/02-Exercices/tp_notes_eleves/main.js
@@ -40,6 +40,46 @@ let students = [
 
 let lessonFields = ['français', 'mathématiques'];
 
+addStudentForm.addEventListener("submit", (data) => {
+    data.preventDefault();
+
+    const newStudent =
+    {
+        lastName: document.querySelector('#student-lastname').value,
+        firstName: document.querySelector('#student-firstname').value
+    };
+
+    if (!students.find(x => x.firstName == newStudent.firstName && x.lastName == newStudent.lastName))
+        students.push(newStudent);
+
+    refreshSelectElements();
+});
+
+addLessonFieldForm.addEventListener("submit", (data) => {
+    data.preventDefault();
+
+    const newLessonField = document.querySelector('#lesson-field').value.toLowerCase();
+    if (!lessonFields.includes(newLessonField))
+        lessonFields.push(newLessonField);
+
+    refreshSelectElements();
+});
+
+addGradeForm.addEventListener("submit", (data) => {
+    data.preventDefault();
+
+    const grade = +document.querySelector('#grade').value;
+    const lessonFieldName = lessonFields[document.querySelector('#grade-field').value-1];
+    const student = students[(+document.querySelector('#grade-student').value) - 1];
+
+    if (!student.grades) student.grades = {};
+    if (!student.grades[lessonFieldName]) student.grades[lessonFieldName] = [];
+
+    student.grades[lessonFieldName].push(grade);
+
+    console.log(students);
+});
+
 addStudentToogleButton.addEventListener("click", () => {
     addStudentVisibility = !addStudentVisibility;
     if (addStudentVisibility) {
@@ -72,3 +112,186 @@ addGradeToogleButton.addEventListener("click", () => {
         addStudentToogleButton.textContent = "ON";
     }
 })
+
+studentChoiceSelect.addEventListener("change", () => {
+    refreshTableElement();
+    refreshAverageGrade();
+});
+
+lessonFieldChoiceSelect.addEventListener("change", () => {
+    refreshTableElement();
+    refreshAverageGrade();
+});
+
+const capitalize = (value) => {
+    value = value.toLowerCase()
+    return value.replace(value[0], value[0].toUpperCase());
+}
+
+const getStudentFullName = (student) => {
+    let firstName = capitalize(student.firstName)
+    let lastName = student.lastName.toUpperCase();
+    return `${firstName} ${lastName}`;
+}
+
+const refreshSelectElements = () => {
+
+    studentChoiceForm.innerHTML = `<option value='0'>Sélectionnez un élève </option>`;
+    for (const student of students) {
+        let option = document.createElement("option")
+        option.value = +students.indexOf(student) + 1
+        option.textContent = getStudentFullName(student)
+        studentChoiceForm.appendChild(option)
+    }
+
+    lessonFieldChoiceForm.innerHTML = `<option value='0'>Sélectionnez une matière</option>`;
+    for (const field of lessonFields) {
+        let option = document.createElement("option")
+        option.value = +lessonFields.indexOf(field) + 1
+        option.textContent = capitalize(field)
+        lessonFieldChoiceForm.appendChild(option)
+    }
+
+    studentChoiceSelect.innerHTML = `<option value='0'>Toutes la classe</option>`;
+    for (const student of students) {
+        let option = document.createElement("option")
+        option.value = +students.indexOf(student) + 1
+        option.textContent = getStudentFullName(student)
+        studentChoiceSelect.appendChild(option)
+    }
+
+    lessonFieldChoiceSelect.innerHTML = `<option value='0'>Toutes les matières</option>`;
+    for (const field of lessonFields) {
+        let option = document.createElement("option")
+        option.value = +lessonFields.indexOf(field) + 1
+        option.textContent = capitalize(field)
+        lessonFieldChoiceSelect.appendChild(option)
+    }
+}
+
+const refreshTableElement = () => {
+    tableDatas.innerHTML = "";
+
+    if (+studentChoiceSelect.value == 0 && +lessonFieldChoiceSelect.value == 0) {
+        for (const student of students) {
+            for (const lessonfield in student.grades) {
+                for (const grade of student.grades[lessonfield]) {
+                    let ligne = document.createElement("tr")
+                    ligne.innerHTML =
+                        `
+                         <td>${student.lastName}</td>
+                         <td>${student.firstName}</td>
+                         <td>${lessonfield.replace(lessonfield[0], lessonfield[0].toUpperCase())}</td>
+                         <td>${grade}</td>
+                     `
+                    tableDatas.appendChild(ligne);
+                }
+            }
+        }
+    } else if (lessonFieldChoiceSelect.value == 0) {
+        const student = students[+studentChoiceSelect.value - 1];
+
+        for (const lessonfield in student.grades) {
+            for (const grade of student.grades[lessonfield]) {
+                let ligne = document.createElement("tr")
+                ligne.innerHTML =
+                    `
+                    <td>${student.lastName}</td>
+                    <td>${student.firstName}</td>
+                    <td>${lessonfield.replace(lessonfield[0], lessonfield[0].toUpperCase())}</td>
+                    <td>${grade}</td>
+                    `
+                tableDatas.append(ligne);
+            }
+        }
+    } else if (studentChoiceSelect.value == 0) {
+        const lessonfield = lessonFields[+lessonFieldChoiceSelect.value-1];
+
+        for (const student of students) {
+
+            for (const grade of student.grades[lessonfield]) {
+                let ligne = document.createElement("tr")
+                ligne.innerHTML =
+                    `
+                    <td>${student.lastName}</td>
+                    <td>${student.firstName}</td>
+                    <td>${lessonfield.replace(lessonfield[0], lessonfield[0].toUpperCase())}</td>
+                    <td>${grade}</td>
+                    `
+                tableDatas.append(ligne);
+            }
+        }
+    } else {
+        const student = students[+studentChoiceSelect.value - 1];
+        const lessonfield = lessonFields[+lessonFieldChoiceSelect.value-1];
+        for (const grade of student.grades[lessonfield]) {
+            let ligne = document.createElement("tr")
+                ligne.innerHTML =
+                    `
+                    <td>${student.lastName}</td>
+                    <td>${student.firstName}</td>
+                    <td>${lessonfield.replace(lessonfield[0], lessonfield[0].toUpperCase())}</td>
+                    <td>${grade}</td>
+                    `
+            tableDatas.append(ligne);
+        }
+    }
+
+}
+
+const calculateAverage = (grades) => {
+    let sum = 0;
+    for (const grade of grades) sum += grade;
+    return (sum / grades.length).toFixed(1);
+}
+
+const refreshAverageGrade = () => {
+    let currentGrades = [];
+
+    if (studentChoiceSelect.value == 0 && lessonFieldChoiceSelect.value == 0) {
+        for (const student of students) {
+            for (const lessonfield in student.grades) {
+                for (const grade of student.grades[lessonfield]) {
+                    currentGrades.push(grade);
+                    averageGradeOutput.innerHTML = `Moyenne générale de la classe : <b>${calculateAverage(currentGrades)}</b>`;
+                }
+            }
+        }
+    } else if (lessonFieldChoiceSelect.value == 0) {
+        const student = students[studentChoiceSelect.value - 1];
+
+        for (const lessonfield in student.grades) {
+            for (const grade of student.grades[lessonfield]) {
+                currentGrades.push(grade);
+                averageGradeOutput.innerHTML = `Moyenne générale de <b>${getStudentFullName(student)}</b> : <b>${calculateAverage(currentGrades)}</b>`;
+            }
+        }
+    } else if (studentChoiceSelect.value == 0) {
+        const lessonfield = lessonFields[lessonFieldChoiceSelect.value-1]
+
+        for (const student of students) {
+            for (const grade of student.grades[lessonfield]) {
+                currentGrades.push(grade);
+                averageGradeOutput.innerHTML = `Moyenne en <b>${capitalize(lessonfield)}</b> de la classe : <b>${calculateAverage(currentGrades)}</b>`;
+
+            }
+        }
+    } else {
+        const student = students[studentChoiceSelect.value - 1];
+        const lessonfield = lessonFields[lessonFieldChoiceSelect.value-1];
+
+        for (const grade of student.grades[lessonfield]) {
+            currentGrades.push(grade);
+            averageGradeOutput.innerHTML = `Moyenne en <b>${capitalize(lessonfield)}</b> de <b>${getStudentFullName(student)}</b> : <b>${calculateAverage(currentGrades)}</b>`;
+        }
+    }
+
+    console.log(currentGrades);
+}
+
+refreshSelectElements();
+
+let test = "1"
+console.log(typeof test)
+let test2 = +test
+console.log(typeof test2)
