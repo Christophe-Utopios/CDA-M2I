063611bd49c1fabf97d9348dc5917bf42288a526
Antoine_Utopios <antoine.dieudonne@labom2iformation.fr>
2023-10-02 12:00:05 +0200

    Demo Styling conditionnel / modules

--- React/demo-bases/src/App.jsx
+++ React/demo-bases/src/App.jsx
@@ -1,9 +1,29 @@
-import DemoListingComponent from "./components/DemoListingComponent";
+import classes from './App.module.css'
+
+import BoutonComponent from "./components/BoutonComponent";
+import DemoVarInject from "./components/DemoVarInject";
 
 function App() {
+  const randomButtonColor = () => {
+    if (Math.random() >= 0.5) {
+      return "red"
+    } 
+
+    return "gray"
+  }
+
   return (
     <div className="App">
-      <DemoListingComponent />
+      <BoutonComponent textBouton="Bonjour"/> <br />
+      <BoutonComponent textBouton="Au revoir"/> <br />
+      <BoutonComponent couleurBouton="blue"/> <br />
+      <BoutonComponent couleurBouton="green"/> <br />
+      <BoutonComponent couleurBouton={randomButtonColor()}/>
+      <img src="" alt="" />
+
+      <p className={classes.paragrapheJaune}>
+        Ce paragraphe vient de App.jsx
+      </p>
       
     </div>
   );
