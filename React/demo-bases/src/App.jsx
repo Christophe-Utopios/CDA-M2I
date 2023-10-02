2cebe652920cef02a6afbe2d39c5465e710eeaf4
Antoine_Utopios <antoine.dieudonne@labom2iformation.fr>
2023-10-02 10:08:21 +0200

    Demo bases React

---
+++ React/demo-bases/src/App.jsx
@@ -0,0 +1,13 @@
+import FirstComponent from "./components/FirstComponent";
+import DemoTableRow from "./components/DemoTableRow";
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+      
+    </div>
+  );
+}
+
+export default App;
