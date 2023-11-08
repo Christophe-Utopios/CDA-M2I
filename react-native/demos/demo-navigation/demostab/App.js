59bf77069a2b271d268a587cce3d04c10d3f3b32
Antoine_Utopios <antoine.dieudonne@labom2iformation.fr>
2023-11-08 15:06:55 +0100

    demo tab navigation

---
+++ react-native/demos/demo-navigation/demostab/App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+// package necessaire navigation :
+// npm install @react-navigation/native
+// npm install react-native-screens react-native-safe-area-context
+// npm i @react-navigation/bottom-tabs
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
