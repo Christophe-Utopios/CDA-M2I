391450f9e456de475fcd4f616a11f396a25595e7
Antoine_Utopios <antoine.dieudonne@labom2iformation.fr>
2023-11-08 11:30:18 +0100

    fin correction calculatrice

--- react-native/exercices/calculator/component/Calculatrice.js
+++ react-native/exercices/calculator/component/Calculatrice.js
@@ -13,6 +13,47 @@ export default function Calculatrice() {
         setFirstNumber('0')
     }
 
+    function operationPress(operateur){
+        console.log("appuie sur l'operateur "+operateur)
+        setOperation(operateur)
+        setSecondNumber(firstNumber)
+        setFirstNumber("")
+    }
+
+    function numberPress(number){
+        console.log("appuie sur  "+number)
+        if (firstNumber != "0") {
+            setFirstNumber(firstNumber + number)
+        } else {
+            setFirstNumber(number)
+        }
+        
+    }
+    function resultPress() {
+        console.log("faire le calcul")
+        console.log("faire l'opération "+secondNumber + " " + operation +" "+ firstNumber)
+        let resultat
+        switch (operation) {
+            case "+":
+                resultat = parseFloat(secondNumber) + parseFloat(firstNumber)
+                setFirstNumber(resultat)
+                break;
+            case "-":
+                resultat = parseFloat(secondNumber) - parseFloat(firstNumber)
+                setFirstNumber(resultat)
+                break;
+            case "/":
+                resultat = parseFloat(secondNumber) / parseFloat(firstNumber)
+                setFirstNumber(resultat)
+                break;
+            case "x":
+                resultat = parseFloat(secondNumber) * parseFloat(firstNumber)
+                setFirstNumber(resultat)
+                break;
+                    
+        }
+    }
+
 
 
   return (
@@ -21,34 +62,34 @@ export default function Calculatrice() {
       <Text style={styles.displayText}>{firstNumber}</Text>
       </View>
       <View style={styles.row}>
-        <Button title={"AC"}></Button>
+        <Button onPress={clear} title={"AC"}></Button>
         <Button title={"^"}></Button>
         <Button title={"%"}></Button>
-        <Button title={"/"}></Button>
+        <Button onPress={() => operationPress("/")} title={"/"}></Button>
       </View>
       <View style={styles.row}>
-        <Button title={"7"} isWhite></Button>
-        <Button title={"8"} isWhite></Button>
-        <Button title={"9"} isWhite></Button>
-        <Button title={"X"}></Button>
+        <Button onPress={() => numberPress("7")}  title={"7"} isWhite></Button>
+        <Button onPress={() => numberPress("8")}  title={"8"} isWhite></Button>
+        <Button onPress={() => numberPress("9")}  title={"9"} isWhite></Button>
+        <Button onPress={() => operationPress("x")} title={"X"}></Button>
       </View>
       <View style={styles.row}>
-        <Button title={"4"} isWhite></Button>
-        <Button title={"5"} isWhite></Button>
-        <Button title={"6"} isWhite></Button>
-        <Button title={"-"}></Button>
+        <Button onPress={() => numberPress("4")}  title={"4"} isWhite></Button>
+        <Button onPress={() => numberPress("5")}  title={"5"} isWhite></Button>
+        <Button onPress={() => numberPress("6")}  title={"6"} isWhite></Button>
+        <Button onPress={() => operationPress("-")} title={"-"}></Button>
       </View>
       <View style={styles.row}>
-        <Button title={"1"} isWhite></Button>
-        <Button title={"2"} isWhite></Button>
-        <Button title={"3"} isWhite></Button>
-        <Button title={"+"}></Button>
+        <Button onPress={() => numberPress("1")}  title={"1"} isWhite></Button>
+        <Button onPress={() => numberPress("2")}  title={"2"} isWhite></Button>
+        <Button onPress={() => numberPress("3")}  title={"3"} isWhite></Button>
+        <Button onPress={() => operationPress("+")} title={"+"}></Button>
       </View>
       <View style={styles.row}>
         <Button title={"."} isWhite></Button>
-        <Button title={"0"} isWhite></Button>
-        <Button title={"Del"} isWhite></Button>
-        <Button title={"="}></Button>
+        <Button onPress={() => numberPress("0")}  title={"0"} isWhite></Button>
+        <Button onPress={clear} title={"Del"} isWhite></Button>
+        <Button onPress={resultPress}  title={"="}></Button>
       </View>
       
     </View>
